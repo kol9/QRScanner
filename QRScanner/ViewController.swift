7f12501e8c76ee298acaf40c6f269a8b0280a052
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2020-02-15 18:23:05 +0300

    Initial Commit

---
+++ QRScanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QRScanner
+//
+//  Created by Nikolay Yarlychenko on 15.02.2020.
+//  Copyright © 2020 Nikolay Yarlychenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
